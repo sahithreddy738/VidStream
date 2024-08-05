2fd3407a8108c6b02393e5bff17aea2bfbda7d30
Sahith Reddy <sahithreddy738@gmail.com>
2024-08-05 11:44:02 +0530

    watch-page added

--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -12,7 +12,6 @@ const useVideos = () => {
    const fetchVideos=async () =>{
     const res=await fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key="+YOUTUBE_KEY);
     const jsonData=await res.json();
-    console.log(jsonData.items);
     dispatch(addPopularVideos(jsonData.items));
    }
 }
